44b4c7755bf05d81ccfe9df29c867eb14e36c8ed
iwstg <iwstg@naver.com>
2025-01-13 18:19:19 +0900

    MVC 디자인 및 구현 2단계

--- src/main/java/springtest/controller/LoginPageController.java
+++ src/main/java/springtest/controller/LoginPageController.java
@@ -1,9 +1,11 @@
 package springtest.controller;
 
+import jakarta.transaction.UserTransaction;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import springtest.data.dto.UserDTO;
 import springtest.data.entity.UserEntity;
 import springtest.data.repository.UserRepository;
@@ -21,7 +23,7 @@ public class LoginPageController {
 
     // 메인페이지 접근 -> 로그인페이지로
     @GetMapping("/")
-    public String MainLoginPage(){
+    public ModelAndView MainLoginPage() {
         //MainLoginPage(Model model, @SessionAttribute(name = "userId", required = false) String userId)
 //        model.addAttribute("loginType", "session-login");
 //        model.addAttribute("pageName", "세션 로그인");
@@ -37,21 +39,20 @@ public class LoginPageController {
         //세션 확인 후 진행
 
         //로그인 로직
-        return "/LoginPage";
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("LoginPage.html");
+        return mav;
     }
 
-    // 로그인페이지 접근 -> 회원가입 페이지로
-    @GetMapping("RegistPage")
-    public String RegistPage(){
-
-
-        return null;
-    }
+    // 로그인페이지 접근 -> 회원가입 페이지로 리다이랙션
+//    @GetMapping("RegistPage")
+//    public String RegistPage(){
+//        return ":thymleafversion/RegistPage";
+//    }
 
     // 회원가입
-    @PostMapping("/RegistPageData")
-    public boolean register(@RequestParam UserDTO userDTO){
-        // System.out.printf("id: %s\nemail: %s\nname: %s\npw: %s\npwcheck: %s", userId, userName, userEmail, userPassword, userPasswordcheck);
+    @PostMapping("/RegistPage")
+    public String register(UserDTO userDTO){
 //        if (userPassword.equals(userPasswordcheck)){
 //            UserDTO userinfo = new UserDTO(userId, userName, userEmail, userPassword);
 //            loginservice.Register(userinfo);
@@ -63,12 +64,22 @@ public class LoginPageController {
 
         if (loginservice.Register(userDTO)){
             System.out.println("[CONTROLLER] - 회원가입 성공");
-            return true;
+            return "MainPage";
         } else {
             System.out.println("[CONTROLLER] - 회원가입 실패");
-            return false;
+            return "RegistPage";
         }
     }
 
-
+    @GetMapping("/testpage")
+    public String testPage(Model model, UserDTO resuserDTO){
+        model.addAttribute("DTO", resuserDTO);
+        return "testpage";
+    }
+    // 테스트
+    @PostMapping("/testpage")
+    public String testpage(@ModelAttribute("DTO") UserDTO resuserDTO){
+        System.out.println(resuserDTO.toString());
+        return "";
+    }
 }
--- src/main/java/springtest/data/dto/UserDTO.java
+++ src/main/java/springtest/data/dto/UserDTO.java
@@ -8,6 +8,37 @@ import lombok.*;
 @ToString
 @Builder
 public class UserDTO {
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
 
     private String userId;
     private String userName;
